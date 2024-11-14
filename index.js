53f36fa470c210c637176660f5acd833b065b33e
Xiannaaaa <xiannasyd@gmail.com>
2024-11-14 17:09:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["dsc.gg/blossmhaven", "Happy shopping patrons!", "🎧 Listening to EXO Albums"];
+const statusTypes = [ 'dnd', 'dnd', 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
